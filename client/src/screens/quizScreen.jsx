6b8b58b1ca03f6dc1ba608ced12faa91dda365e4
Taylor <Taylor Stubbs>
2022-09-28 15:19:47 +0100

    Added quizScreen.css

--- client/src/screens/quizScreen.jsx
+++ client/src/screens/quizScreen.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import '../App.css';
+import React, { useEffect } from 'react';
+import './quizScreen.css';
 // import Button from '@mui/material/Button';
 import { ButtonGroup, Button } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
@@ -17,10 +17,16 @@ function QuizScreen() {
       },
     },
   });
+
+  // const [question, setQuestion] = useState();
+
+  useEffect(() => {
+  }, []);
+
   return (
     <div className="App">
       <img src={image} alt="logo" style={{ height: '12rem', width: '38rem', margin: '3rem' }} />
-      <p className="Question">Space For Question</p>
+      <div className="Question" />
       <ButtonGroup varient="text">
         <Button theme={theme} color="primary" varient="contained" sx={{ color: '#252F3E' }}>ANSWER 1</Button>
         <Button theme={theme} color="primary" varient="contained" sx={{ color: '#252F3E' }}>ANSWER 2</Button>

d04740df7cd48b62bc49ae8a8a516c8dcce1dfb3
Taylor <Taylor Stubbs>
2022-10-07 10:36:08 +0100

    Added not found screen test

---
+++ client/src/tests/NotFoundScreen.test.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import React from 'react';
+import NotFoundScreen from '../screens/NotFoundScreen';
+
+describe('Not found screen', () => {
+  beforeEach(() => {
+    render(
+      <NotFoundScreen />,
+    );
+  });
+
+  it('renders \'404 Not Found\' image', () => {
+    const notFoundImage = screen.getByTestId('not-found-image');
+    expect(notFoundImage).toBeTruthy();
+  });
+});
